4bb2bbee4b1c49b66afa52ec8b2165b68ba6ce1e
ShubhamWP07 <shubham1900384@gmail.com>
2023-05-24 18:34:21 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useState } from "react";
+import "./App.css";
 
-function App() {
+// Step 1: Create a new context
+const ThemeContext = createContext();
+
+// Step 2: Create a provider component
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// Step 3: Create a consumer hook
+const useTheme = () => useContext(ThemeContext);
+
+// Step 4: Create components that consume the theme
+const Dashboard = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={`dashboard ${theme}-theme`}>
+      <h1>Dashboard</h1>
+      <p>Current theme: {theme}</p>
+      <button className="btn" onClick={toggleTheme}>
+        Toggle Theme
+      </button>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Dashboard />
+    </ThemeProvider>
+  );
+};
 
 export default App;
